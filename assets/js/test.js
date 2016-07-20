c97a271fc074c24cbdc689111791c23f08551fb6
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-07-20 11:56:21 -0400

    add rest of modified files so we can merge

--- assets/js/test.js
+++ assets/js/test.js
@@ -13,3 +13,30 @@ app.controller('testController', ['$http', '$scope', function($http, $scope) {
 
     });
 }]);
+// original code above
+// app.controller('testController', ['$http', '$scope', function($http, $scope) {
+//
+//
+//     $http({
+//         method: 'GET',
+//         // url: 'https://681ca077.ngrok.io/businesses/1.json'
+//         url: 'https://livelocalrails.herokuapp.com/businesses.json',
+//         data: {"location": zipcode},
+//     }).success(function call(data) {
+//
+//         $scope.businesses = userList;
+//
+//         console.log(data);
+//
+//     });
+// }]);
+
+// //////////////////
+// $.ajax({
+//   method: 'GET',
+//   url: 'http://74be7da1.ngrok.io/my_bookmarks',
+//   data: { "username": plockUser, "password": plockPassword},
+// })
+// // .success returns the data from the url above, console.log proves that it made a successful connection
+//   .success(function (data) {
+//   console.log("success");

79deb4a57513c523c1bd14118c0d8c018819198d
Jordan Long <jordan.long222@gmail.com>
2016-07-21 15:04:54 -0400

    fix url

--- assets/js/test.js
+++ assets/js/test.js
@@ -7,7 +7,7 @@ app.controller('testController', ['$http', '$scope', function($http, $scope) {
         method: 'GET',
         // url: 'https://681ca077.ngrok.io/businesses/1.json'
         url: 'https://livelocalrails.herokuapp.com/businesses.json',
-        headers: {'Authorization':'asdf'}
+        headers: {'Authorization':token}
 
     }).success(function call(userList) {
         $scope.businesses = userList.businesses;

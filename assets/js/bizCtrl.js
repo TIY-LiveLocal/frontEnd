66772a8e8109ee44170abba09d229c22a1c49e75
Jordan Long <jordan.long222@gmail.com>
2016-07-21 09:40:57 -0400

    add styling changes for login/dashboard, fix urls

--- assets/js/bizCtrl.js
+++ assets/js/bizCtrl.js
@@ -1,12 +1,14 @@
-app.controller('bizProfController', ['$http', '$scope', function($http, $scope) {
+app.controller('bizController', ['$http', '$scope', function($http, $scope) {
 
 
     $http({
         method: 'GET',
-        url: 'https://livelocalrails.herokuapp.com/businesses.json'
+        url: 'https://livelocalrails.herokuapp.com/find.json',
+        data: {location:27701},
+        headers:{'Authorization': 'asdf'}
       }).success(function call(userList) {
 
-          $scope.businesses = userList;
+          // $scope.businesses = userList;
 
           console.log(userList);
 

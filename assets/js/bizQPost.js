716933933cd6af1df41e00874866b988e061c431
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-07-26 09:44:55 -0400

    Sucessfully making POST request to backend for question-asking on the business profile page. However, getting authorization error. Next step: work through that with backend.

--- assets/js/bizQPost.js
+++ assets/js/bizQPost.js
@@ -1,5 +1,31 @@
 //make a POST request so users can submit questions to the business's Page
 //make it show up dynamically with ng-show
+//will have to pass the business_id and
 
 
-app.controller('bizQPost', ['$http', '$scope', function($http, $scope)])
+app.controller('bizQPost', ['$http', '$scope', function($http, $scope){
+
+  console.log('question posting is ready');
+
+// var businessIdNo = how do we go about pulling this information out?
+
+$(document).on('click', '#askButton', function (e){
+  e.preventDefault();
+  console.log('ask has been clicked');
+
+var questionText = $('input[id="qASectionHead"]').val();
+
+  console.log('The user has typed: ' + questionText);
+
+      $http({
+        url: 'https://livelocalrails.herokuapp.com/questions.json',
+        method: 'POST',
+        data: {question_text: questionText},
+        headers: {'token': 'df'},
+      }).success(function(data){
+
+  });
+
+});
+
+}]);

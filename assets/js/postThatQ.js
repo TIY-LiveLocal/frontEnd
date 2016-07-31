1363aaad8868c1d83b945cd010e5324602ca4245
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-07-31 14:06:05 -0400

    Add dynamic post listing to page combined with submission of POST request

--- assets/js/postThatQ.js
+++ assets/js/postThatQ.js
@@ -1,35 +1,43 @@
+app.controller('qCtrl', ['$http', '$scope', function($http, $scope) {
 
-app.controller('qCtrl', ['$http', '$scope',  function($http, $scope) {
+    console.log('question posting is ready');
+    var questionText = $('input[id="qASectionHead"]').val();//will grab what user types in search bar and pass in POST request
+    var businessIdNo = localStorage.getItem('phone');//will need to assign db content to specific businesses
+    var token = localStorage.getItem('token');//pulling user token for authorization purposes in POST request
 
-                console.log('question posting is ready');
-                var questionText = $('input[id="qASectionHead"]').val();
-                var businessIdNo = localStorage.getItem('phone');
-                var token = localStorage.getItem('token');
+    console.log(token);
+    console.log(businessIdNo);
 
-                console.log(token);
-                console.log(businessIdNo);
+      $scope.posts = [] //empty array to push post into so user can see it
 
-                $scope.askButton = function() {
-                  console.log('ask has been clicked');
+    $scope.askButton = function() { //when "ask" button is clicked....
+        console.log('ask has been clicked');
 
-                      qData = {
-                          qText: $("#qASectionHead").val()
-                      }
+        qData = {
+            qText: $("#qASectionHead").val()
+        }
 
-                    console.log('user has typed ' + $("#qASectionHead").val());
 
-                    $http({
-                        url: ' https://291a9b88.ngrok.io/questions',
-                        method: 'POST',
-                        data: {
-                            question_text: questionText,
-                            phone: businessIdNo,
-                        },
-                        headers: {'Authorization': token},
-                    }).success(function(data) {
+        $scope.posts.push({
+            'content': $scope.newPost, //push what user types so it's reflected on the page
+        })
+        $scope.newPost = ''
 
-                        console.log(data);
-                        });
-                      };
-                    // });
-                    }]);
+        console.log('user has typed ' + $("#qASectionHead").val());
+
+        $http({
+            url: ' https://livelocalrails.herokuapp.com/questions', //post request to backend
+            method: 'GET',
+            data: {
+                question_text: questionText,
+                phone: businessIdNo,
+            },
+            headers: {
+                'Authorization': token
+            },
+        }).success(function(data) {
+
+            console.log(data);
+        });
+    }
+}]);

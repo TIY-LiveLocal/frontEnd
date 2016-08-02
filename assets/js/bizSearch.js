3cd562b1f5f5b970bff48dc8042a40fc20567595
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-08-02 10:26:34 -0400

    Add functionality for the business search feature. Will pull in results from yelp API as well as display them. Next step styling.

--- assets/js/bizSearch.js
+++ assets/js/bizSearch.js
@@ -1,5 +1,5 @@
 
-app.controller('bizSearchCtrl', ['$http', '$scope', function($http, $scope){
+app.controller('bizSearchCtrl', ['$scope','$location','$http', function($scope, $location, $http){
 
 var token = localStorage.getItem('token');
 var bizNameTerm = $('input[id="bizNameHere"]').val();
@@ -9,18 +9,76 @@ var bizNameLoc  = $('input[id="bizAddressHere"]').val();
 
     console.log('search has been clicked');
     console.log('user has typed '+ $('input[id="bizNameHere"]').val());
+    console.log('user has typed '+ $('input[id="bizAddressHere"]').val());
 
     $http({
       method: 'GET',
       url: 'https://livelocalrails.herokuapp.com/find_business',
-      // url: 'https://9da6162d.ngrok.io/find_business',
+      // url: 'https://7bf05740.ngrok.io/find_business',
       headers: {'Authorization': token},
-      params: {'term': bizNameTerm,
-                'location': bizNameLoc}
+      params: {'term': $('input[id="bizNameHere"]').val(),
+                'location': $('input[id="bizAddressHere"]').val()}
       }).then(function successCallback(response){
+        $scope.businesses = response.data; //placed at the right bracket of the response, helps define response and tells it what you need
         console.log(response);
       }, function errorCallback (response){
 
       });
+
+      //click & get index of clicked dataThang
+        $scope.logID = function(){
+          var vm = this.biz;
+
+          myDataThing = $scope.businesses;
+      //an object
+        console.log(typeof myDataThing);
+
+          clicked = $(this).myDataThing;
+
+      //logs the object information that has been clicked
+      //click & get index of clicked dataThang
+        $scope.logID = function(){
+          var vm = this.biz;
+
+          myDataThing = $scope.businesses;
+      //an object
+        console.log(typeof myDataThing);
+
+          clicked = $(this).myDataThing;
+
+      //logs the object information that has been clicked
+        console.log(vm, vm.location[0]);
+
+        localStorage.setItem('name',vm.name);
+        localStorage.setItem('address',vm.address);
+        localStorage.setItem('city',vm.city);
+        localStorage.setItem('zip',vm.zip_code);
+        localStorage.setItem('phone',vm.phone);
+        localStorage.setItem('business_id', vm.id);
+        localStorage.setItem('username', vm.user_id);
+        localStorage.setItem('longitude',vm.location[1]);
+        localStorage.setItem('latitude',vm.location[0]);
+
+      //redirect user to business profile dashboard page
+          $location.path('/business');
+          return vm;
+        };
+
+        localStorage.setItem('name',vm.name);
+        localStorage.setItem('address',vm.address);
+        localStorage.setItem('city',vm.city);
+        localStorage.setItem('zip',vm.zip_code);
+        localStorage.setItem('phone',vm.phone);
+        localStorage.setItem('business_id', vm.id);
+        localStorage.setItem('username', vm.user_id);
+        localStorage.setItem('longitude',vm.location[1]);
+        localStorage.setItem('latitude',vm.location[0]);
+
+      //redirect user to business profile dashboard page
+          $location.path('/business');
+          return vm;
+        };
+
     };
+
 }]);
